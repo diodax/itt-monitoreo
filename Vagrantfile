4efea21ab76291f5c802d4b65537b516c59c02f8
diodax <diosdavi20@gmail.com>
2016-09-05 17:45:27 -0400

    primer commit

---
+++ Vagrantfile
@@ -0,0 +1,53 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  # Switched boxes due to this bug: https://github.com/mitchellh/vagrant/issues/6426#issuecomment-234977448
+  # config.vm.box = "hashicorp/precise64"
+  config.vm.box = "ubuntu/precise64"
+
+  # config.vm.network :forwarded_port, guest: 3000, host: 3000, auto_correct: true
+  # Add port-forward for Redis
+  config.vm.network :forwarded_port, guest: 6379, host: 6379 #, auto_correct: true
+  # Add port-forward for Node-inspector
+  config.vm.network :forwarded_port, guest: 5858, host: 5858 #, auto_correct: true
+  # Add port-forward Sails.js application
+  config.vm.network :forwarded_port, guest: 1337, host: 1337 #, auto_correct: true
+  # Add port-forward for the MongoDB service
+  config.vm.network :forwarded_port, guest: 27017, host: 27017 #, auto_correct: true
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  config.vm.network :private_network, ip: "192.168.33.10"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network :public_network
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  config.vm.synced_folder ".", "/home/vagrant", create: true
+
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # VirtualBox:
+  config.vm.provider :virtualbox do |vb|
+    # Use VBoxManage to customize the VM. To change memory:
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+
+  # Enable provisioning with Puppet stand alone.  Puppet manifests
+  # are contained in a directory path relative to this Vagrantfile.
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.module_path    = "puppet/modules"
+    puppet.manifest_file  = "main.pp"
+    puppet.options        = [
+                              '--verbose',
+                              #'--debug',
+                            ]
+  end
+end
---
+++ puppet/manifests/main.pp
@@ -0,0 +1,98 @@
+class apt_update {
+    exec { "aptGetUpdate":
+        command => "sudo apt-get update",
+        path => ["/bin", "/usr/bin"]
+    }
+}
+
+class othertools {
+    package { "git":
+        ensure => latest,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "vim-common":
+        ensure => latest,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "curl":
+        ensure => present,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "htop":
+        ensure => present,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "g++":
+        ensure => present,
+        require => Exec["aptGetUpdate"]
+    }
+}
+
+class nodejs {
+  file { '/vagrant':
+    ensure => 'directory',
+  }
+
+  exec { "git_clone_n":
+    command => "git clone https://github.com/tj/n.git /home/n",
+    creates => '/home/n/Makefile',
+    path => ["/bin", "/usr/bin"],
+    require => [Exec["aptGetUpdate"], Package["git"], Package["curl"], Package["g++"]]
+  }
+
+  exec { "install_n":
+    command => "make install",
+    path => ["/bin", "/usr/bin"],
+    cwd => "/home/n",
+    require => Exec["git_clone_n"]
+  }
+
+  exec { "install_node":
+    command => "n latest",
+    path => ["/bin", "/usr/bin", "/usr/local/bin"],
+    require => [Exec["git_clone_n"], Exec["install_n"]]
+  }
+
+  exec { "npm-update":
+    # cwd => "/vagrant",
+    command => "npm -g update",
+    #onlyif => ["test -d /vagrant/node_modules"],
+    path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"],
+    require => [Exec["install_node"], File['/vagrant']]
+  }
+
+  exec { "npm-sails":
+    # cwd => "/vagrant",
+    command => "sudo npm -g install sails",
+    #onlyif => ["test -d /vagrant/node_modules"],
+    path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"],
+    require => [Exec["install_node"], File['/vagrant'], Exec["npm-update"]]
+  }
+}
+
+class mongodb {
+  class {'::mongodb::globals':
+    manage_package_repo => true,
+    bind_ip             => ["127.0.0.1"],
+  }->
+  class {'::mongodb::server':
+    port    => 27017,
+    verbose => true,
+    ensure  => "present"
+  }->
+  class {'::mongodb::client': }
+}
+
+class redis-cl {
+  class { 'redis': }
+}
+
+include apt_update
+include othertools
+include nodejs
+include mongodb
+include redis-cl
