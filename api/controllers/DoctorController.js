7a8942bda8d63c7e23e910a78a318402445ac9cf
diodax <diosdavi20@gmail.com>
2016-10-20 00:25:06 -0400

    redireccionamiento del DashboardCOntroller y vista basica del DoctorController

--- api/controllers/DoctorController.js
+++ api/controllers/DoctorController.js
@@ -6,6 +6,21 @@
  */
 
 module.exports = {
-	
+    /**
+     * `DoctorController.index()`
+     */
+    index: function(req, res) {
+				Doctor.find()
+							.populate('user')
+							.populate('patients')
+							.exec(function (err, doctors) {
+								if (err) {
+							    return res.serverError(err);
+							  }
+								return res.view({
+									user: req.user.toJSON(),
+									doctors: doctors
+								});
+							});
+    },
 };
-
